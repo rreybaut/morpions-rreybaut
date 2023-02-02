87cda76126d3e83fc3cb1ebbaea563ad1bfcc8b4
raphael reybaut <rreybaut@gmail.com>
2023-02-02 17:26:50 +0100

    suite ok

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    value: null,
-  };
-}
+// Supprimez le constructor de Square, puisqu’il ne maintient plus son propre état local ->
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     value: null,
+//   };
+// }
 
+  // remplacer {this.state.value} par this.props.value
+  // Remplacez this.setState() par this.props.onClick() dans la méthode render de Square
   render() {
     return (
       <button
       className="square"
-      onClick={() => this.setState({value: 'X'})}
+      onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -26,11 +29,27 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Ajout constructeur  Board qui définiton état initial en 1 tableau de 9 nulls pour les 9 cases
       squares: Array(9).fill(null),
     };
   }
+
+  // Lorsque nous cliquons sur un Square, nous devrions obtenir une erreur parce que nous n’avons pas encore défini handleClick. Nous allons donc l’ajouter dans la classe Board :
+handleClick(i) {
+  const squares = this.state.squares.slice();
+  squares[i] = 'X';
+  this.setState({squares: squares});
+}
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        // Nous passons désormais deux props de Board à Square : value et onClick
+        value={this.state.squares[i]}
+        // fonction de Board au Square, qui sera appelée par Square en réponse aux clics
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
